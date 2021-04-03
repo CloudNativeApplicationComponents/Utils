c567d4ea58ae0523f126b75bfe1af77438cb255d
r.taroosheh <r.taroosheh@alibaba.ir>
2021-04-03 21:30:12 +0430

    add ServiceCollectionConfigurator

---
+++ src/CloudNativeApplicationComponents.Utils/Configurators/ServiceCollectionConfigurator.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace CloudNativeApplicationComponents.Utils.Configurators
+{
+    public class ServiceCollectionConfigurator
+    {
+        public IServiceCollection ServiceCollection { get; }
+
+        public ServiceCollectionConfigurator(IServiceCollection collection)
+        {
+            ServiceCollection = collection
+                ?? throw new ArgumentNullException(nameof(collection));
+        }
+    }
+}
